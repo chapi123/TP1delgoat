47d3479da6ca5475fe9172e95fcf713c4b754563
AlvarezF <schaparro@alumno.huergo.edu.ar>
2026-04-09 15:47:54 -0300

    dia 2- funciones para botones y interfaz de playlist y canciones

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # TP1delgoat
 DIA 1 8/4
 Cree el repositorio gamer
+Cree toda la interfaz donde tengo botones y controles, pausa, play, stop, shuffle, next, y previous
+
+DIA 2 9/4
+le di funciones a los botones
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import customtkinter as ctk
 import tkinter as tk  
 import pygame
-from PIL import ImageTk, Image 
+from PIL import ImageTk, Image
+from tkinter import font
+import os
+import random
+
+#pygame.mixer.init()
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("dark-blue")
@@ -20,7 +25,7 @@ left_panel = ctk.CTkFrame(main, width=250, fg_color="#121212")
 left_panel.pack(side="left", fill="y")
 left_panel.pack_propagate(False)
 
-title_playlists = ctk.CTkLabel(left_panel, text="Playlists", font=("Montserrat", 18))
+title_playlists = ctk.CTkLabel(left_panel, text="Playlists", font=("Montserrat", 18), text_color="#FFFFFF")
 title_playlists.pack(pady=10)
 
 separator = ctk.CTkFrame(main, width=2, fg_color="#2a2a2a")
@@ -29,7 +34,7 @@ separator.pack(side="left", fill="y")
 right_panel = ctk.CTkFrame(main, fg_color="#121212")
 right_panel.pack(side="left", fill="both", expand=True)
 
-title_current = ctk.CTkLabel(right_panel, text="Playlist actual", font=("Montserrat", 20))
+title_current = ctk.CTkLabel(right_panel, text="Playlist actual", font=("Montserrat", 20), text_color="#FFFFFF")
 title_current.pack(pady=10)
 
 
@@ -63,16 +68,94 @@ loop2_icon = ImageTk.PhotoImage(loop2_img)
 playing = False
 shuffle = False
 loop = False
+playlist = []
+current_song = 0
+history = []
+
+def load_playlist () :
+    playlist_path = "playlists"
+    
+    if not os.path.exists(playlist_path):
+        return
+    
+    for folder in os.listdir(playlist_path) :
+        full_path = os.path.join(playlist_path,folder)
+        
+        if os.path.isdir(full_path) :
+            btn = ctk.CTkButton(
+                left_panel,
+                text=folder,
+                fg_color="#1a1a1a",
+                hover_color="#2a2a2a",
+                text_color="#FFFFFF",
+                font=("Montserrat", 12),
+                command=lambda path=full_path: open_playlist(path)
+            )
+            btn.pack(fill="x", padx="10", pady="5")
+
+def open_playlist(path):
+    global playlist, current_song
+    
+    clear_right_panel()
+    
+    playlist = []
+    current_song = 0
+    
+    for file in os.listdir(path):
+        if file.endswith(".mp3"):
+            full_path = os.path.join(path, file)
+            playlist.append(full_path)
+            
+            btn = ctk.CTkButton(
+                right_panel,
+                text=file,
+                fg_color="#1a1a1a",
+                hover_color="#2a2a2a",
+                anchor="w",
+                command=lambda p=full_path: play_selected(p)
+            )
+            btn.pack(fill="x", padx=10, pady=3)
+    
+    if playlist:
+        pygame.mixer.music.load(playlist[0])
+
+def play_selected(path):
+    global current_song, playing
+
+    new_index = playlist.index(path)
+
+    if playing and current_song != new_index:
+        history.append(current_song)
+    
+    pygame.mixer.music.stop()
+    current_song = playlist.index(path)
+    pygame.mixer.music.load(path)
+    pygame.mixer.music.play()
+
+    btn_play.configure(image=pause_icon)
+    playing = True
+
+def clear_right_panel():
+    for widget in right_panel.winfo_children():
+        widget.destroy()
 
 def toggle_play():
     global playing
     
+    if not playlist :
+        return   
+
     playing = not playing
-    
+
     if playing:
+        pygame.mixer.music.pause()
         btn_play.configure(image=pause_icon)
+        playing = False
+
     else:
+        pygame.mixer.music.unpause()
         btn_play.configure(image=play_icon)
+        playing = True
 
 def toggle_shuffle():
     global shuffle
@@ -93,6 +176,47 @@ def toggle_loop():
         btn_loop.configure(image=loop2_icon)
     else:
         btn_loop.configure(image=loop1_icon)
+
+def stop() :
+    global playing
+
+    pygame.mixer.music.stop()
+    btn_play.configure(image=play_icon)
+    playing = False
+
+def next_song():
+    global current_song
+    
+    if not playlist:
+        return
+    
+    if shuffle:
+        import random
+        new_index = random.randint(0, len(playlist) - 1)
+    else:
+        new_index = (current_song + 1) % len(playlist)
+    
+    play_selected(playlist[new_index])  
+ 
+    
+
+def prev_song():
+    global current_song, playing
+    
+    if not history:
+        pygame.mixer.music.stop()
+        btn_play.configure(image=play_icon)
+        playing = False
+        return
+    
+    current_song = history.pop()
+    
+    pygame.mixer.music.stop()
+    pygame.mixer.music.load(playlist[current_song])
+    pygame.mixer.music.play()
+    
+    btn_play.configure(image=pause_icon)
+    playing = True
 
 def on_enter(e):
     btn_stop.configure(image=stop2_icon)
@@ -122,7 +246,8 @@ btn_backward = ctk.CTkButton(
     height=40,
     fg_color="#181818",      
     hover_color="#181818",   
-    border_width=0               
+    border_width=0,
+    command=prev_song            
 )
 btn_backward.pack(side="left", padx= 10, pady=15)
 
@@ -159,7 +284,8 @@ btn_stop = ctk.CTkButton(
     height=40,
     fg_color="#181818",      
     hover_color="#181818",   
-    border_width=0,          
+    border_width=0, 
+    command=stop         
 )
 btn_stop.pack(side="left", padx= 10, pady=15)
 btn_stop.bind("<Enter>", on_enter)
@@ -177,5 +303,7 @@ btn_loop = ctk.CTkButton(
     command=toggle_loop           
 )
 btn_loop.pack(side="left", padx= 10, pady=15)
+
+load_playlist()
 
 root.mainloop()
